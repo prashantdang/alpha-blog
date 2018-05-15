735e6ada03e91fdad71dfedddfdba0c0e60f0b34
Prashant Dang <prashantdang@gmail.com>
2018-05-15 08:08:14 +0000

    Add users listing and articles view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-  def gravtar_for(user, options = { size: 80  })
+  def gravatar_for(user, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options [:size]
-    gravatar_url = "https//secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
-  
 end
